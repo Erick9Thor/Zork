9bfdb25928d45257dc93467fe0cb08b79e46d89d
Erick9Thor <erictoper@gmail.com>
2021-03-08 18:50:58 +0100

    [ADD] First esquema for Entity class based on guide

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "../include/World.h";
+
+using namespace std;
 
 int main() {
+
+    welcomeMessage();
+
+    World World;
+
+
+    while (1) {
+
+    }
     return 0;
+}
+
+void welcomeMessage() {
+    cout << "Welcome to Zork!----------------\n";
 }
