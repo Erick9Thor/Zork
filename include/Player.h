e84d6dfa88ee239ba75368f4c4f92469c4bede7d
Erick9Thor <erictoper@gmail.com>
2021-03-10 19:34:00 +0100

    [ADD] first class definition of player and actions

--- include/Player.h
+++ include/Player.h
@@ -1,7 +1,25 @@
 #pragma once
 #include "Creature.h"
+
+
 class Player :
     public Creature
 {
+    public:
+        Player(string name, string description, Room* location) : Creature(EntityType::PLAYER, name, description, location) {
+
+        }
+
+        // PLAYER ACTIONS
+		void Talk(const string& str);
+		bool Attack(const string& str);
+		void Go(const string& str);
+		void Inspect(const string& str);
+		void Take(const string& str);
+		void Equip(const string& str);
+		void Drop(const string& str);
+		void Unequip(const string& str);
+		void Unlock(const string& str);
+		void Loot(const string& str);
 };
 
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,1 +1,42 @@
 #include "../include/Player.h"
+
+void Player::Talk(const string& str)
+{
+}
+
+bool Player::Attack(const string& str)
+{
+    return false;
+}
+
+void Player::Go(const string& str)
+{
+}
+
+void Player::Inspect(const string& str)
+{
+}
+
+void Player::Take(const string& str)
+{
+}
+
+void Player::Equip(const string& str)
+{
+}
+
+void Player::Drop(const string& str)
+{
+}
+
+void Player::Unequip(const string& str)
+{
+}
+
+void Player::Unlock(const string& str)
+{
+}
+
+void Player::Loot(const string& str)
+{
+}
